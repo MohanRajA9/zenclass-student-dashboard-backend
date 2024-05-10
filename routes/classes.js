562514d6900f018567dc393e4d4f10ea6c0b241b
MohanRajA9 <akmohanraj555@gmail.com>
2024-05-10 20:31:51 +0530

    updated

--- routes/classes.js
+++ routes/classes.js
@@ -5,7 +5,7 @@ import { auth } from "../middleware/auth.js";
 
 const router = express.Router();
 
-router.get("/getClassById/:id", auth, async (req,res)=>{
+router.get("/getClassById/:id", async (req,res)=>{
     const {id} = req.params;
     // console.log(id, typeof(id),ObjectId(+id));
     const classData = await (await client.db("zenStudentDashboard").collection("classes").find({_id: ObjectId(id)}).toArray());
@@ -14,7 +14,7 @@ router.get("/getClassById/:id", auth, async (req,res)=>{
 })
 
 
-router.get("/getAllClasses", auth, async (req,res)=>{
+router.get("/getAllClasses", async (req,res)=>{
     const classesData = await client.db("zenStudentDashboard").collection("classes").find().sort({_id : 1}).toArray();
     console.log(classesData);
     classesData.length ? res.send(classesData) : res.status(404).send({message : "Classes Data not found"});
