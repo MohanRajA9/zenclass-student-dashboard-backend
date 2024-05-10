723cb80ae9165f145e2ab3fdee16637fa80455e5
MohanRajA9 <akmohanraj555@gmail.com>
2024-05-10 19:27:44 +0530

    data updated

--- data.js
+++ data.js
@@ -1,53 +1,98 @@
 export const courseData = [
     {
        "course" : "HTML",
-        "coverPage" : "https://tse1.mm.bing.net/th/id/OIP.DFQ5RvH7YoayuVPeAFiWogHaEo?pid=ImgDet&rs=1",
+        "coverPage" : "https://www.oxfordwebstudio.com/user/pages/06.da-li-znate/sta-je-html/sta-je-html.jpg",
         "courseURL" : "https://www.youtube.com/embed/N8YMl4Ezp4g",
         "description" : "HTML is the language behind every website on the Internet! It's a markup language used to define the structure and content of web pages. It's the first language you need to learn if you want to become a web developer."
     },
     {
         "course" : "CSS",
-         "coverPage" : "https://codecondo.com/wp-content/uploads/2014/03/11-Background-CSS-Pattern-Generators.jpg",
+         "coverPage" : "https://www.oxfordwebstudio.com/user/pages/06.da-li-znate/sta-je-css/sta-je-css.png",
          "courseURL" : "https://www.youtube.com/embed/r1xBCi5SOjw",
          "description" : "Cascading Style Sheets (CSS) is a stylesheet language used to describe the presentation of a document written in HTML or XML (including XML dialects such as SVG, MathML or XHTML). CSS describes how elements should be rendered on screen, on paper, in speech, or on other media."
      },
      {
         "course" : "JAVA SCRIPT",
-         "coverPage" : "https://tse1.mm.bing.net/th/id/OIP.ZEu_2nVjitpB-jhLRgfn4AHaEK?pid=ImgDet&rs=1",
+         "coverPage" : "https://bairesdev.mo.cloudinary.net/blog/2023/08/What-Is-JavaScript-Used-For.jpg?tx=w_1920,q_auto",
          "courseURL" : "https://www.youtube.com/embed/XIOLqoPHCJ4",
          "description" : "JavaScript is the world most popular lightweight, interpreted compiled programming language. It is also known as scripting language for web pages. It is well-known for the development of web pages, many non-browser environments also use it. JavaScript can be used for Client-side developments as well as Server-side developments"
      },
      {
         "course" : "REACT JS",
-         "coverPage" : "https://wallpaperaccess.com/full/3949100.png",
+         "coverPage" : "https://miro.medium.com/v2/resize:fit:720/format:webp/1*dLaDL-lSN0iprzmOpmM7zQ.png",
          "courseURL" : "https://www.youtube.com/embed/w7ejDZ8SWv8",
          "description" : "React is a declarative, efficient, and flexible JavaScript library for building user interfaces. ReactJS is an open-source, component-based front-end library responsible only for the view layer of the application. It is maintained by Facebook."
      },
      {
         "course" : "MONGO DB",
-         "coverPage" : "https://www.flynerd.pl/wp-content/uploads/2020/11/mongoDB_podstawy.png",
+         "coverPage" : "https://lapsql.wordpress.com/wp-content/uploads/2021/09/mongodb.jpg",
          "courseURL" : "https://www.youtube.com/embed/-56x56UppqQ",
          "description" : "MongoDB, the most popular NoSQL database, is an open-source document-oriented database. The term NoSQL means non-relational. It means that MongoDB isn't based on the table-like relational database structure but provides an altogether different mechanism for storage and retrieval of data. This format of storage is called BSON ( similar to JSON format). "
      },
      {
         "course" : "NODE JS",
-         "coverPage" : "https://tse4.mm.bing.net/th/id/OIP.ri547X8343ggZrqjx56UWAHaEK?pid=ImgDet&rs=1",
+         "coverPage" : "https://andrewbeeken.co.uk/wp-content/uploads/2018/11/nodejs.jpg?w=816",
          "courseURL" : "https://www.youtube.com/embed/qz0aGYrrlhU",
          "description" : "Node.js is an open-source and cross-platform runtime environment for executing JavaScript code outside a browser. You need to remember that NodeJS is not a framework and it's not a programming language. Most people are confused and understand it's a framework or a programming language."
      },
      {
         "course" : "Express JS",
-        "coverPage" : "https://codecondo.com/wp-content/uploads/2015/10/Express-Frameworks_805.pngs",
+        "coverPage" : "https://media.licdn.com/dms/image/D5612AQHqYWqhiP2bWQ/article-cover_image-shrink_720_1280/0/1713769258709?e=1720656000&v=beta&t=-ioNnlyE-UHngTiDTkvlEKtZr-u63-eksbO5OEjQ1nM",
         "courseURL" : "https://www.youtube.com/watch?v=SccSCuHhOw0",
         "description" : "Express.js is a small framework that works on top of Node.js web server functionality to simplify its APIs and add helpful new features. It makes it easier to organize your application’s functionality with middleware and routing. It adds helpful utilities to Node.js HTTP objects and facilitates the rendering of dynamic HTTP objects."
     }
 ]
 
+// const courseDataDummy = [
+//     {
+//        "course" : "HTML",
+//         "coverPage" : "https://tse1.mm.bing.net/th/id/OIP.DFQ5RvH7YoayuVPeAFiWogHaEo?pid=ImgDet&rs=1",
+//         "courseURL" : "https://www.youtube.com/embed/N8YMl4Ezp4g",
+//         "description" : "HTML is the language behind every website on the Internet! It's a markup language used to define the structure and content of web pages. It's the first language you need to learn if you want to become a web developer."
+//     },
+//     {
+//         "course" : "CSS",
+//          "coverPage" : "https://codecondo.com/wp-content/uploads/2014/03/11-Background-CSS-Pattern-Generators.jpg",
+//          "courseURL" : "https://www.youtube.com/embed/r1xBCi5SOjw",
+//          "description" : "Cascading Style Sheets (CSS) is a stylesheet language used to describe the presentation of a document written in HTML or XML (including XML dialects such as SVG, MathML or XHTML). CSS describes how elements should be rendered on screen, on paper, in speech, or on other media."
+//      },
+//      {
+//         "course" : "JAVA SCRIPT",
+//          "coverPage" : "https://tse1.mm.bing.net/th/id/OIP.ZEu_2nVjitpB-jhLRgfn4AHaEK?pid=ImgDet&rs=1",
+//          "courseURL" : "https://www.youtube.com/embed/XIOLqoPHCJ4",
+//          "description" : "JavaScript is the world most popular lightweight, interpreted compiled programming language. It is also known as scripting language for web pages. It is well-known for the development of web pages, many non-browser environments also use it. JavaScript can be used for Client-side developments as well as Server-side developments"
+//      },
+//      {
+//         "course" : "REACT JS",
+//          "coverPage" : "https://wallpaperaccess.com/full/3949100.png",
+//          "courseURL" : "https://www.youtube.com/embed/w7ejDZ8SWv8",
+//          "description" : "React is a declarative, efficient, and flexible JavaScript library for building user interfaces. ReactJS is an open-source, component-based front-end library responsible only for the view layer of the application. It is maintained by Facebook."
+//      },
+//      {
+//         "course" : "MONGO DB",
+//          "coverPage" : "https://www.flynerd.pl/wp-content/uploads/2020/11/mongoDB_podstawy.png",
+//          "courseURL" : "https://www.youtube.com/embed/-56x56UppqQ",
+//          "description" : "MongoDB, the most popular NoSQL database, is an open-source document-oriented database. The term NoSQL means non-relational. It means that MongoDB isn't based on the table-like relational database structure but provides an altogether different mechanism for storage and retrieval of data. This format of storage is called BSON ( similar to JSON format). "
+//      },
+//      {
+//         "course" : "NODE JS",
+//          "coverPage" : "https://tse4.mm.bing.net/th/id/OIP.ri547X8343ggZrqjx56UWAHaEK?pid=ImgDet&rs=1",
+//          "courseURL" : "https://www.youtube.com/embed/qz0aGYrrlhU",
+//          "description" : "Node.js is an open-source and cross-platform runtime environment for executing JavaScript code outside a browser. You need to remember that NodeJS is not a framework and it's not a programming language. Most people are confused and understand it's a framework or a programming language."
+//      },
+//      {
+//         "course" : "Express JS",
+//         "coverPage" : "https://codecondo.com/wp-content/uploads/2015/10/Express-Frameworks_805.pngs",
+//         "courseURL" : "https://www.youtube.com/watch?v=SccSCuHhOw0",
+//         "description" : "Express.js is a small framework that works on top of Node.js web server functionality to simplify its APIs and add helpful new features. It makes it easier to organize your application’s functionality with middleware and routing. It adds helpful utilities to Node.js HTTP objects and facilitates the rendering of dynamic HTTP objects."
+//     }
+// ]
+
 
 export const classesData = [
     {
         "day" : "Day-01",
-        "date" : "08/30/2022",
+        "date" : "29/04/2024",
         "time" : "10:00 AM - 12:30 PM",
         "classTopic" : "Hyper Text Markup Language",
         "KeyAreas" : ["HTML Elements","HTML Attributes","HTML Headings","HTML Formatting Elements", "HTML Tags", "HTML Forms"],
@@ -57,7 +102,7 @@ export const classesData = [
     },
     {
         "day" : "Day-02",
-        "date" : "09/01/2022",
+        "date" : "30/04/2024",
         "time" : "10:00 AM - 12:30 PM",
         "classTopic" : "Cascading Style Sheets",
         "KeyAreas" : ["CSS Syntax","Selectors","Box Model","CSS Flex", "CSS Grid", "CSS Position Properties"],
@@ -67,7 +112,7 @@ export const classesData = [
     },
     {
         "day" : "Day-03",
-        "date" : "09/05/2022",
+        "date" : "01/05/2024",
         "time" : "10:00 AM - 12:30 PM",
         "classTopic" : "Java Script",
         "KeyAreas" : ["JS Syntax","var, let & const","Operators","Functions", "Objects", "Classes"],
@@ -77,7 +122,7 @@ export const classesData = [
     },
     {
         "day" : "Day-04",
-        "date" : "09/01/2022",
+        "date" : "02/05/2024",
         "time" : "10:00 AM - 12:30 PM",
         "classTopic" : "Java Script",
         "KeyAreas" : ["API calls","Promise","fetch","async & await", "DOM"],
@@ -87,7 +132,7 @@ export const classesData = [
     },
     {
         "day" : "Day-05",
-        "date" : "09/12/2022",
+        "date" : "03/05/2024",
         "time" : "10:00 AM - 12:30 PM",
         "classTopic" : "React JS",
         "KeyAreas" : ["Why React","Componet","Functional Components","Hooks", "Life Cycle Methods", "React Router"],
@@ -97,7 +142,7 @@ export const classesData = [
     },
     {
         "day" : "Day-06",
-        "date" : "09/12/2022",
+        "date" : "06/05/2024",
         "time" : "10:00 AM - 12:30 PM",
         "classTopic" : "Mongo DB",
         "KeyAreas" : ["Data Base","Collections & documents","Advantages of Mongo DB","CRUD operations on Mongo DB", "Aggregation methods of Mongo DB"],
@@ -107,7 +152,7 @@ export const classesData = [
     },
     {
         "day" : "Day-07",
-        "date" : "09/12/2022",
+        "date" : "07/05/2024",
         "time" : "10:00 AM - 12:30 PM",
         "classTopic" : "Node JS & Express JS",
         "KeyAreas" : ["Server Architecture","fs commands","Express Setup","HTML Formatting Elements", "HTML Tags", "HTML Forms"],
@@ -122,52 +167,52 @@ export const classesData = [
 export const taskData = [
     {
         "taskName" : "Task-01",
-        "taskInfo" : "https://github.com/RaghuRamAnnamdasu/Capstone-tasks/blob/main/Task%20-%2001.jpg - Implement UI as shown in provided URL",
+        "taskInfo" : "https://docs.google.com/document/d/1lkzy8u0rkW5v2jGvbJcJrmVVLjMqN_TmmsHkL41AoIc/preview - Please read the task description in provided URL",
         "taskDoneBy" : []
     },
     {
         "taskName" : "Task-02",
-        "taskInfo" : "https://github.com/RaghuRamAnnamdasu/Capstone-tasks/blob/main/Task-02.docx - Please read the task description in provided URL",
+        "taskInfo" : "From the below Title, you can pick any two and complete them with responsive design. For your reference visit this URL https://github.com/rvsp/HTML-CSS/tree/master/Tasks",
         "taskDoneBy" : []
     },
     {
         "taskName" : "Task-03",
-        "taskInfo" : "https://github.com/RaghuRamAnnamdasu/Capstone-tasks/blob/main/Task-03.docx - Please read the task description in provided URL",
+        "taskInfo" : "Pagination task: https://docs.google.com/document/d/1O8yVCwB_GaY5rZK1_mNkZ9GeYJLeom_dYmcVN3Mb2ws/edit - Please read the task description in provided URL",
         "taskDoneBy" : []
     },
     {
         "taskName" : "Task-04",
-        "taskInfo" : "https://startbootstrap.com/snippets/pricing-table - Implement pricecart UI as in the mentioned URL using React JS",
+        "taskInfo" : "Callback Hell task: https://docs.google.com/document/d/1K6jW6TPmqWvzoRAm2t-GlOAxKmMRsLt5zaCsdcH_3LM/edit - Please read the task description in provided URL",
         "taskDoneBy" : []
     },
     {
         "taskName" : "Task-05",
-        "taskInfo" : "https://startbootstrap.com/previews/shop-homepage - Implement UI as per the url and add functionality between 'Add to Cart' button and 'Cart'.",
+        "taskInfo" : "Promise task- https://docs.google.com/document/d/1wa9laCdb__QIty4ZAre2-uY0OmHeq4xinhKpwjty8UI/preview - Please read the task description in provided URL",
         "taskDoneBy" : []
     },
     {
         "taskName" : "Task-06",
-        "taskInfo" : "Implement CRUD operation with different UI of admin dashboard for Student mangement and use formik for all form validations.",
+        "taskInfo" : "React price card task- https://docs.google.com/document/d/11URBvZUsXT4Sr3YtCuv-5gcOHmkOmX976h-QKcXbWqs/edit - Please read the task description in provided URL",
         "taskDoneBy" : []
     },
     {
         "taskName" : "Task-07",
-        "taskInfo" : "https://github.com/RaghuRamAnnamdasu/Capstone-tasks/blob/main/Task-07.docx - Please read the task description in provided URL",
+        "taskInfo" : "React shopping cart task- https://docs.google.com/document/d/1mqZqoxmPlEioKCwVfuOYIUcG2kESrYuocrYO4h9kiRk/edit - Please read the task description in provided URL",
         "taskDoneBy" : []
     },
     {
         "taskName" : "Task-08",
-        "taskInfo" : "Implement 'Forgot Password' page using nodemailer concept.",
+        "taskInfo" : "https://docs.google.com/document/d/1yfQTicBMNV7thyzewyYSJWhTNw0pC4AkVhMGVCIP0LU/edit - Please read the task description in provided URL",
         "taskDoneBy" : []
     },
     {
         "taskName" : "Task-09",
-        "taskInfo" : "Implement a student mentor portal using MERN stack.",
+        "taskInfo" : "Nodejs file system Task https://docs.google.com/document/d/1c_u6azHv7AQHse45GGcrCRe78liwv90S5aHwBR7tBP0/edit - Please read the task description in provided URL",
         "taskDoneBy" : []
     },
     {
         "taskName" : "Task-10",
-        "taskInfo" : "Implement a MERN stack project of Car rental system.",
+        "taskInfo" : "Nodejs hall booking api task https://docs.google.com/document/d/1rwPQ2LbHtMZajA_GIZfR-Ko2MFueoT82AmfHTK9V-hM/edit - Please read the task description in provided URL",
         "taskDoneBy" : []
     }
 ]
