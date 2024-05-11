904058e3f1dc2e6e5c46b7906d11daa17c9cd275
MohanRajA9 <akmohanraj555@gmail.com>
2024-05-11 10:46:18 +0530

    updated

--- index.js
+++ index.js
@@ -18,6 +18,7 @@ app.use(
           credentials: true
         })
       );
+      
 // app.use((req, res, next) => {
 //         res.setHeader("Access-Control-Allow-Origin", "*");
 //         res.header(
