7ab815191cdb753c446bc506b4dc0ca877d6de4e
MohanRajA9 <akmohanraj555@gmail.com>
2024-05-26 02:17:48 +0530

    correction for delete method

--- routes/helper.js
+++ routes/helper.js
@@ -13,6 +13,6 @@ export async function createQuery(data) {
   return await client.db("zenStudentDashboard").collection("queries").insertOne(data);
   }
 
-  export async function deleteQuery(id) {
-    return client.db("firstDatabase").collection("products").deleteOne({_id: ObjectId(id)});
-  }
+  // export async function deleteQuery(id) {
+  //   return client.db("firstDatabase").collection("products").deleteOne({_id: ObjectId(id)});
+  // }
